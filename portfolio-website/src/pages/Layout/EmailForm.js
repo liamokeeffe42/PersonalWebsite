23d40dccc3aa4f0ef49e46fdf9f4faa4baf5a95e
liamkokeeffe <okeeffel@uw.edu>
2022-11-08 22:02:47 -0800

    finished footer elements

--- portfolio-website/src/pages/Layout/EmailForm.js
+++ portfolio-website/src/pages/Layout/EmailForm.js
@@ -1,20 +1,83 @@
 import React from 'react';
+import { useState } from 'react';
+import { send } from 'emailjs-com';
 
 import "./EmailForm.css";
 
 function EmailForm() {
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        message: '',
+        reply_to: '',
+        phone_number: ''
+    });
+
+    const [hasSubmit, setHasSubmit] = useState(false);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+            `${process.env.REACT_APP_SERVICE_ID}`,
+            `${process.env.REACT_APP_TEMPLATE_ID}`,
+            toSend,
+            `${process.env.REACT_APP_USER_ID}`
+        )
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+                setHasSubmit(true);
+                resetFormValues();
+            })
+            .catch((err) => {
+                console.log('FAILED...', err);
+                resetFormValues();
+            });
+    };
+
+    function resetFormValues() {
+        setToSend({
+            from_name: '',
+            message: '',
+            reply_to: '',
+            phone_number: ''
+        });
+    }
+
+    const onSubmitAgain = (e) => {
+        setHasSubmit(false);
+    }
+
+    const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value});
+    };
+
     return (
-        <form id="footer-form">
-            <input type="text" id="footer-input-name" name="name" className="footer-input" placeholder="Name" />
-            <label for="footer-input-name" className="footer-label">Name <span className="footer-input-required">*</span></label>
-            <input type="text" id="footer-input-email" name="email" className="footer-input" placeholder="Email" />
-            <label for="footer-input-email" className="footer-label">Email <span className="footer-input-required">*</span></label>
-            <input type="text" id="footer-input-phone" name="phoneNumber" className="footer-input" placeholder="Number" />
-            <label for="footer-input-phone" className="footer-label">Phone Number</label>
-            <textarea type="text" id="footer-input-textarea" name="message" className="footer-input" placeholder="Message" />
-            <label for="footer-input-textarea" id="footer-label-textarea">Message <span className="footer-input-required">*</span></label>
-            <button id="footer-btn-submit">SEND</button>
-        </form>
+        <div id="container-emailform" style={{
+            display: hasSubmit ? 'flex' : 'block',
+            alignItems: hasSubmit ? 'center' : '',
+            justifyContent: hasSubmit ? 'center' : ''
+        }}>
+            <form id="container-emailform-form" onSubmit={onSubmit}
+                style={{
+                    display: hasSubmit ? 'none' : 'block'
+                }}>
+                <input type="text" id="emailform-input-name" name="from_name" value={toSend.from_name} onChange={handleChange} className="emailform-input" placeholder="Name" required />
+                <label for="emailform-input-name" className="emailform-label">Name <span className="emailform-input-required">*</span></label>
+                <input type="email" id="emailform-input-email" name="reply_to" value={toSend.reply_to} onChange={handleChange} className="emailform-input" placeholder="Email" required />
+                <label for="emailform-input-email" className="emailform-label">Email <span className="emailform-input-required">*</span></label>
+                <input type="text" id="emailform-input-phone" name="phone_number" value={toSend.phone_number} onChange={handleChange} className="emailform-input" placeholder="Number" />
+                <label for="emailform-input-phone" className="emailform-label">Phone Number</label>
+                <textarea type="text" id="emailform-input-textarea" name="message" value={toSend.message} onChange={handleChange} className="emailform-input" placeholder="Message" required />
+                <label for="emailform-input-textarea" id="emailform-label-textarea">Message <span className="emailform-input-required">*</span></label>
+                <button id="emailform-btn-submit" type="submit">SEND</button>
+            </form>
+            <div id="container-emailform-submitted"
+                style={{
+                    display: hasSubmit ? 'block' : 'none'
+                }}>
+                <p>Thanks for the message!</p>
+                <button onClick={onSubmitAgain}>Submit Another</button>
+            </div>
+        </div>
     );
 }
 
--- portfolio-website/src/pages/Layout/Socials.js
+++ portfolio-website/src/pages/Layout/Socials.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import facebookIcon from "../../assets/imgs/icon-facebook.png";
 import githubIcon from "../../assets/imgs/icon-github.png";
@@ -7,9 +8,15 @@ import linkedinIcon from "../../assets/imgs/icon-linkedin.png";
 function Socials() {
     return (
         <div id="container-footer-socials">
-            <img className="footer-icon-social" src={facebookIcon} alt="Facebook Icon" />
-            <img className="footer-icon-social" src={githubIcon} alt="GitHub Icon" />
-            <img className="footer-icon-social" src={linkedinIcon} alt="LinkedIn Icon" />
+            <a href="https://www.facebook.com/liam.okeeffe.7503" className="footer-social-link" target="_blank">
+                <img className="footer-icon-social" src={facebookIcon} alt="Facebook Icon" />
+            </a>
+            <a href="https://github.com/liamokeeffe42" className="footer-social-link" target="_blank">
+                <img className="footer-icon-social" src={githubIcon} alt="GitHub Icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/l-okeeffe/" className="footer-social-link" target="_blank">
+                <img className="footer-icon-social" id="footer-icon-lastsocial" src={linkedinIcon} alt="LinkedIn Icon" />
+            </a>                
         </div>
     );
 }
